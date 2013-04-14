daa36313a84f4e7604afff47263e10e342def338
Hideto Yoshii <blue.snow.sea.world@gmail.com>
2013-04-15 01:02:41 +0900

    Add the how to use the Kirokusyo Machine

--- kirokusyo-machine.rb
+++ kirokusyo-machine.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/ruby
 # -*- coding: utf-8 -*-
+# % ruby kirokusyo-machine.rb oldrecord_file period1 period2
+# % ruby kirokusyo-machine.rb test.org 20130401/20130415 20130415/20130430
 
 require File.expand_path(File.dirname(__FILE__) + '/record_generator.rb')
 
